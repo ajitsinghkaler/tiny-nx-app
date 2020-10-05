afb6386a3e6a78df090081f836c91f319ef6ec3c
Ajit Singh <ajitsinghkaler0@gmail.com>
2020-10-05 17:56:09 +0530

    add debug meta reducer

--- libs/shared/data-access/src/lib/reducers/index.ts
+++ libs/shared/data-access/src/lib/reducers/index.ts
@@ -1,19 +1,12 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
-import { environment } from '../../environments/environment';
+// reducers/index.ts
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { environment } from '@workspace/shared/environments';
+import { debug } from './debug';
 
-export interface State {
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface State {}
 
-}
+export const reducers: ActionReducerMap<State> = {};
 
-export const reducers: ActionReducerMap<State> = {
-
-};
-
-
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<State>[] =
+  !environment.production ? [debug] : [];
