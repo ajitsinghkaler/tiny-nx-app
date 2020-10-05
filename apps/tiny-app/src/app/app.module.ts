5be5e8a79b009db4aa1cd8edb8621f868bb6c392
Ajit Singh <ajitsinghkaler0@gmail.com>
2020-10-05 17:40:00 +0530

    added ngrx

--- apps/tiny-app/src/app/app.module.ts
+++ apps/tiny-app/src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CoreModule} from './core.module';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
